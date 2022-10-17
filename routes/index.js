af04461b11c69b9a5894b76c95a2c8694bef8b16
Seankeane1 <108425807+Seankeane1@users.noreply.github.com>
2022-10-17 12:21:10 -0400

    video 3

--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,19 @@ const express = require("express");
 const router = new express.Router(); //instatiate express router
 
 router.get("/", async (req,res) =>{
+   const teacher = {name:"Whalen", age: 31, tenure: true}
     try{
-        return res.send("We're live");
+        res.send(req.query)
     } catch(error){
         console.log(error);
     }
-})
+});
 
 module.exports = router;
+router.get("/teacher/:name", async (req,res)=>{
+    try {
+        res.json(req.params.name)
+    } catch (error) {
+        console.log(error)
+    }
+});
