448c17b22e64cf9cfe5293cd6480e74475da9062
Seankeane1 <108425807+Seankeane1@users.noreply.github.com>
2022-10-19 12:28:01 -0400

    class

--- Controllers/shopController.js
+++ Controllers/shopController.js
@@ -1,11 +1,13 @@
 exports.middlewareSample = (req, res, next) =>{
-    
+    req.name = "TEST";
+    next();
 }
 
 
 exports.homepage = async (req,res) =>{
     const stores = ["Dunkin", "Tim Hortons", "Seven Eleven"];
      try{
+         console.log(req.name)
          res.json(stores);
      } catch(error){
          console.log(error);
