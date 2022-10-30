9fa8002495d7197a9b3007ade4268d87d07c69f0
SeanKeane1 <seankean36@gmail.com>
2022-10-29 22:23:19 -0400

    L6
    I couldn't figure out authentication and I'm too tired to try anymore today

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const port = process.env.PORT || 3000; // localhost:3000
 const app = express();
+require("./DB/mongoose"); // makes sure mongoose connects and runs
 const routes = require("./Routes/index");
 //takes raw requests and turns them into useable properties on req.body
 app.use(express.json());
