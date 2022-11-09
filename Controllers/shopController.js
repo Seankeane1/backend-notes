ecee15b3f0a71f6fd0368dc388c4e4d696bbe379
SeanKeane1 <seankean36@gmail.com>
2022-11-09 11:26:58 -0500

    Done
    actually deleting data

--- Controllers/shopController.js
+++ Controllers/shopController.js
@@ -30,13 +30,25 @@ exports.homepage = async (req,res) =>{
     }
  };
 
- exports.updateShops = async(req, res)=> {
+ exports.updateShop = async(req, res)=> {
     try {
       const shop = await shop.findById(req.params.id);
       const updates = Object.keys(req.body);
       updates.forEach((update)=> (shop[update] = req.body[update]));
       await shop.save()
       res.json(shop);
+    } catch (error) {
+        console.log(error)
+    }
+ };
+
+ exports.deleteShop = async (req, res)=> {
+    try {
+        const shop = await shop.findByIdAndDelete(req.params.id);
+        if(!shop){
+            res.status(404).send();
+        }
+        res.send(`${shop.name} was deleted from the DB`)
     } catch (error) {
         console.log(error)
     }
